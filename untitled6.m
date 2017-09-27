f0ebbf80d7e45c6f392316c69a24484808f1ab4f
zarg129 <32308286+zarg129@users.noreply.github.com>
2017-09-27 04:14:24 +0300

    Update untitled6.m

--- untitled6.m
+++ untitled6.m
@@ -1,8 +1,9 @@
 fs = 1000;
-t = 0:1/fs:2;           %1/fs krok (period deskret)
+t = 0:1/fs:2;                               %1/fs krok (period deskret)
 x = 3*sin(6*pi*t)+5*sin(16*pi*t);
-pks = pksdetect1(x);       %calling func
-figure(4)
-plot(t,x,'k-');            %k- style of line
-hold on;                    %hold this style
-plot(t(pks),x(pks),'k*');   % /-/
+  pks = pksdetect1(x);                        %calling func
+  figure(4)
+  plot(t,x,'k-');                             %k- style of line
+  hold on;                                    %hold this style
+  plot(t(pks),x(pks),'k*');hold off;          % /-/ off this style to blue
+  xlabel(’time (s)’); ylabel(’x(t)’);         % names of ox and oy
